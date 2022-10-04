72054ff881d09783166578fbef4faf2bb6b95c82
Dokina <dokina@Dokinas-MacBook-Pro.local>
2022-10-04 23:00:43 +0900

    line width changes in runtime.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
+const lineWidth = document.querySelector("#line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
+ctx.lineWidth = lineWidth.value;
 
 function drawLine(startPosX, startPosY, endPosX, endPosY, lineWidth) {
   ctx.beginPath();
@@ -29,9 +31,16 @@ function onMouseDown(event) {
 
 function onMouseUp(event) {
   isPainting = false;
+  ctx.beginPath();
+}
+
+function onValueChange(event) {
+  ctx.lineWidth = event.target.value;
 }
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", onMouseDown);
 canvas.addEventListener("mouseup", onMouseUp);
 canvas.addEventListener("mouseleave", onMouseUp);
+
+lineWidth.addEventListener("change", onValueChange);
